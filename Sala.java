d4775307cea74682a32ed78b5ad50282c6b2fd60
facucepe <facundocepero.09@gmail.com>
2026-09-02 10:19:38 -0300

    Create Sala.java

---
+++ Sala.java
@@ -0,0 +1,21 @@
+public class Sala {
+    private int id;
+    private String nombre;
+
+    public Sala(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String toString() {
+        return "Sala " + this.id + " - " + this.nombre;
+    }
+}
